31b5f4c10732d21f697815cfb8f908ff992e3e97
Pyunghyuk Yoo <yoophi@gmail.com>
2021-05-03 23:25:05 +0900

    ✨ NEW: add paginate interface

--- todo_app/repositories/interface.py
+++ todo_app/repositories/interface.py
@@ -1,9 +1,38 @@
 from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+from typing import Optional, List
+
+
+@dataclass
+class PaginationDto:
+    page: int
+    per_page: int
+
+
+@dataclass
+class PaginationFilterDto:
+    key: str
+    value: str
+
+
+@dataclass
+class PaginationSortDto:
+    key: str
+    descending: bool
 
 
 class IRepository(metaclass=ABCMeta):
     @abstractmethod
     def get_todo_list(self, limit=10):
+        pass
+
+    @abstractmethod
+    def get_todo_paginate(
+        self,
+        pagination: PaginationDto,
+        filters: List[PaginationFilterDto],
+        sort: Optional[PaginationSortDto] = None,
+    ):
         pass
 
     @abstractmethod
